2138d0cfedc0f95d45d3125075e8a1bc902bf668
themahabbat <mahabbatmusic@gmail.com>
2018-04-28 18:12:25 +0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,64 @@
 var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    babel = require("gulp-babel"),
-    livereload = require("gulp-livereload");
+pug = require("pug"),
+sass = require("gulp-sass"),
+babel = require("gulp-babel"),
+livereload = require("gulp-livereload"),
+pretty = require("pretty"),
+fs = require("fs");
 
-gulp.task("html", function() {
-  gulp.src("*.html").pipe(livereload());
+const buildHTML = (body) => {
+  let header = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  `
+  
+  let footer = `
+  
+  `
+  
+  fs.readFile("./src/assets.json", "utf-8", (err, data) => {
+    data = JSON.parse(data);
+    let css = data["css"];
+    let js = data["js"];
+    
+    css.forEach(url => {
+      header += `<link rel="stylesheet" href="${url}" />`
+    });
+    header += `
+    <link rel="stylesheet" href="bundle/main.css">
+
+    <title>Frontend Development</title>
+    </head><body>
+
+    `;
+    
+    js.forEach(url => {
+      footer += `<script src="${url}"></script>`
+    });
+    footer += `
+    <script src="bundle/main.js"></script>
+    </body>
+    </html>`;
+    
+    const html = pretty(header + body + footer)
+    
+    fs.writeFile("./index.html", html, err => {
+      console.log(err);
+    });
+    
+    
+  });
+  
+}
+
+gulp.task("pug", function () {
+  let html = pug.renderFile('./src/main.pug')
+  buildHTML(html)
+  gulp.src('./src/main.pug').pipe(livereload())
 });
 
 gulp.task("sass", function() {
@@ -27,7 +81,7 @@ gulp
 
 gulp.task("watch", function() {
   livereload.listen();
-  gulp.watch("*.html", ["html"]);
+  gulp.watch("./src/main.pug", ["pug"]);
   gulp.watch("./src/main.scss", ["sass"]);
   gulp.watch("./src/main.js", ["js"]);
 });
